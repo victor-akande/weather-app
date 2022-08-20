5d64718131f459dfc1ee2439312060718d8f6088
Victor AKANDE <victor123akande@gmail.com>
2022-08-19 20:18:07 -0700

    added the first api (Geo-DB API)

---
+++ src/api.js
@@ -0,0 +1,7 @@
+export const geodbApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'f1edd738d3mshfd7fc40f820ace3p13b75djsnbdfc82a640f2',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com' 
+    }
+};
